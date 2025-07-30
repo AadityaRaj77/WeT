ca4452e79ddb101846b2afa3a848558b9ebf0edf
AadityaRaj77 <aaditya200602@gmail.com>
2025-07-30 21:57:04 +0530

    fetch data from api

--- WeT/app/index.tsx
+++ WeT/app/index.tsx
@@ -1,6 +1,37 @@
+import { useState } from "react";
 import { Text, View, Button, TextInput, SafeAreaView, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Index() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState<{
+    temp: number;
+    humidity: number;
+    windSpeed: number;
+  } | null>(null);
+  const [loading, setLoading] = useState(false)
+  const API_KEY = '48ab7588f7de2ea964d935e8edd1fe10';
+  const fetchWeather = async() => {
+    if(city.trim() !== ""){
+      Keyboard.dismiss()
+      try{
+        const res = await fetch( `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${API_KEY}&units=metric`)
+        const json = await res.json();
+        if (res.ok) {
+          setWeather({
+            temp: json.main.temp,                       
+            humidity: json.main.humidity,               
+            windSpeed: json.wind.speed,                
+          });
+        } else {
+          setWeather(null);
+        }
+      } catch (e) {
+        setWeather(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+      };
   return (
       <ScrollView contentContainerStyle={styles.container}>
         <TextInput placeholder="Enter City" style={{ borderWidth: 1, borderColor: "blue", padding: 10, margin: 10, borderRadius: 10 }} />
@@ -19,15 +50,15 @@ export default function Index() {
         }}>Weather</Text>
         <View style={styles.info}>
           <Text style={styles.infohead}>Temperature</Text>
-          <Text style={styles.maininfo}>69 C</Text>
+          <Text style={styles.maininfo}>{weather.temp}°C</Text>
         </View>
         <View style={styles.info}>
           <Text style={styles.infohead}>Humidity</Text>
-          <Text style={styles.maininfo}>69%</Text>
+          <Text style={styles.maininfo}>{weather.humidity}%</Text>
         </View>
         <View style={styles.info}>
           <Text style={styles.infohead}>Wind</Text>
-          <Text style={styles.maininfo}>69 km/hr</Text>
+          <Text style={styles.maininfo}>{weather.windSpeed} m/s</Text>
         </View>
       </ScrollView>
   );
